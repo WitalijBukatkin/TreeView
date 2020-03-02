1dc23747b8359fdcbdc8984376b613a6105f2bb3
vitalijbukatkin <vitaliy.bukatkin@gmail.com>
2020-03-02 19:56:46 +0300

    Initial

---
+++ src/main/java/com/github/witalijbukatkin/treeview/TreeviewApplication.java
@@ -0,0 +1,13 @@
+package com.github.witalijbukatkin.treeview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/witalijbukatkin/treeview/TreeviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.witalijbukatkin.treeview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
